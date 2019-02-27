3c4e1dd59030465751b6420e572ad4210fd6241d
amartinm7 <amartinm7@gmail.com>
2019-02-27 23:27:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+![Building web applications with Spring Boot and Kotlin](./_media/kotlin-spring-boot.png)
+
+# Kotlin-blog 
+taken from spring guide: [Building web applications with Spring Boot and Kotlin](https://spring.io/guides/tutorials/spring-boot-kotlin/)
+
+Springboot Rest API with Kotlin and JPA
+
+```bash
+gradle init --type java-library
+```
+
+```bash
+./gradlew compileKotlin
+```
+
+Your custom properties should now be recognized when editing application.properties (autocomplete, validation, etc.).
+```bash
+./gradlew kaptKotlin
+```
---
+++ src/main/kotlin/com/amm/concept/blog/BlogConfiguration.kt
@@ -0,0 +1,32 @@
+package com.amm.concept.blog
+
+import com.amm.concept.blog.domain.Article
+import com.amm.concept.blog.domain.ArticleRepository
+import com.amm.concept.blog.domain.User
+import com.amm.concept.blog.domain.UserRepository
+import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class BlogConfiguration {
+
+    @Bean
+    fun databaseInitializer(userRepository: UserRepository,
+                            articleRepository: ArticleRepository) = ApplicationRunner {
+
+        val smaldini = userRepository.save(User("smaldini", "Stéphane", "Maldini"))
+        articleRepository.save(Article(
+                title = "Reactor Bismuth is out",
+                headline = "Lorem ipsum",
+                content = "dolor sit amet",
+                author = smaldini
+        ))
+        articleRepository.save(Article(
+                title = "Reactor Aluminium has landed",
+                headline = "Lorem ipsum",
+                content = "dolor sit amet",
+                author = smaldini
+        ))
+    }
+}
